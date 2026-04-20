61623330ed98791d2ff2cbb56142dfacff58312a
Beyond Mobile Health Exec <jovon@mach4labs.com>
2026-04-19 20:28:05 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Apply from './Apply.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Apply />
+  </React.StrictMode>
+)
